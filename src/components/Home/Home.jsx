678d9c896ecf45305bd7319acbf48e95bf8ce1a0
sadusha nilupul <sadushanilupul13@gmail.com>
2026-04-11 00:56:13 +0530

    Update home hero layout and animation

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import HeroImg from '../../assets/coffee2.png';
 
 export const Home = () => {
   return (
-    <section className="container mx-auto px-4 py-16">
-      <h1 className="text-4xl font-bold text-primary">We serve the richest <span className="text-secondary">Coffee</span> in the city.</h1>
-    </section>
+    <div className="min-h-[500px] sm:min-h-[600px] bg-brand-dark flex justify-center items-center text-white">
+      <div className="container pb-8 sm:pb-0">
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-8">
+          <div className="flex flex-col justify-center">
+            <h1 className="text-5xl sm:text-6xl lg:text-7xl font-bold">We serve the richest <span className="text-primary font-cursive">Coffee</span> in the city.</h1>
+            <div className="mt-8">
+              <button className="bg-gradient-to-r from-primary to-secondary border-2 border-primary rounded-full px-4 py-2 text-white hover:scale-150 duration-200">Coffee and Code</button>
+            </div>
+          </div>
+          <div className="flex justify-center items-center">
+            <img src={HeroImg} alt="Coffee" className="w-[300px] sm:w-[450px] sm:scale-110 mx-auto animate-spin-slow" />
+          </div>
+        </div>
+      </div>
+    </div>
+
   );
 };
